63557b87446b62c5bb6808b5e5c7ee9417c33467
Guilherme David <guilhermedavidrk@icloud.com>
2022-09-15 19:47:43 -0300

    rebuilding structure :boom:

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { Checkout } from './pages/checkout'
-import { Home } from './pages/home/index'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
+      <Route path="/" element={<DefaultLayout />}></Route>
     </Routes>
   )
 }
